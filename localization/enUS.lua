3350109548e5fa317558da3dc0145e65e71f2ff7
Bloodsoul <Bloodsoul@users.noreply.github.com>
2016-10-26 15:27:52 +0200

    Add files via upload

---
+++ localization/enUS.lua
@@ -0,0 +1,6 @@
+function locale_us()
+	L["not_in_a_raid_group"] =		"You are not in a raid group."
+	L["cannot_find_section"] =	"TeamSpeak3-entry was not found on guild info page!"
+	L["not_in_a_guild"] =		"You are not in a guild."
+	L["cannot_read_guild_info"] =	"Cannot read guild info page."
+end
