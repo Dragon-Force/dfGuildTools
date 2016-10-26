3d4dfb0de6bce0c4680be2f9cc10b94d20360fbd
Bloodsoul <Bloodsoul@users.noreply.github.com>
2016-10-26 15:27:33 +0200

    Create deDE.lua

---
+++ localization/deDE.lua
@@ -0,0 +1,6 @@
+function locale_de()
+	L["not_in_a_raid_group"] =		"Du befindest dich in keinem Schlachtzug."
+	L["cannot_find_section"] =	"TeamSpeak3-Eintrag in der Gildeninfo nicht gefunden!"
+	L["not_in_a_guild"] =		"Du bist in keiner Gilde."
+	L["cannot_read_guild_info"] =	"Kann Guildeninfo nicht lesen."
+end
